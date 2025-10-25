14ebc7c58515d0a4feb911604938707bad8c0fa7
olgabiriukova <lelia.bir1503@gmail.com>
2025-10-25 18:16:16 +0200

    changelog schema definition and ChangeLogValidator implementation

--- ontology-migration-tool/src/main/java/cz/cvut/fel/loader/ChangeLogLoader.java
+++ ontology-migration-tool/src/main/java/cz/cvut/fel/loader/ChangeLogLoader.java
@@ -3,7 +3,9 @@ package cz.cvut.fel.loader;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import cz.cvut.fel.exceptions.ChangeLogValidationException;
 import cz.cvut.fel.model.ChangeLog;
+import cz.cvut.fel.utils.ChangeLogValidator;
 import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
@@ -11,9 +13,15 @@ import org.yaml.snakeyaml.constructor.Constructor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class ChangeLogLoader {
-    public ChangeLog load(InputStream input) throws IOException {
+    private final ChangeLogValidator validator;
+    public ChangeLogLoader() throws IOException {
+        this.validator = new ChangeLogValidator();
+    }
+    public ChangeLog load(InputStream input) throws IOException, ChangeLogValidationException {
+        validator.validate(input);
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         return mapper.readValue(input, ChangeLog.class);
     }
---
+++ ontology-migration-tool/src/main/java/cz/cvut/fel/utils/ChangeLogValidator.java
@@ -0,0 +1,42 @@
+package cz.cvut.fel.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
+import cz.cvut.fel.exceptions.ChangeLogValidationException;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+
+public class ChangeLogValidator {
+    private final JsonSchema jsonSchema;
+
+    public ChangeLogValidator() throws IOException {
+        InputStream schema = ChangeLogValidator.class.getResourceAsStream("changelog-schema.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode node = objectMapper.readTree(schema);
+        JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V202012);
+        this.jsonSchema = factory.getSchema(node);
+    }
+
+    public void validate(InputStream input) throws IOException, ChangeLogValidationException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        JsonNode changeLogNode = mapper.readTree(input);
+        Set<ValidationMessage>  errors = jsonSchema.validate(changeLogNode);
+        if(!errors.isEmpty()) {
+            throw new ChangeLogValidationException("ChangeLogValidation error", errors);
+        }
+
+
+    }
+
+
+}
